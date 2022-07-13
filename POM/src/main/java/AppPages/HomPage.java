1a89b3306e080f47038cc166831d09a70e0fd79e
Mounisha.M <Mounisha.M@SEZLP657.avasoft.com>
2022-07-14 00:42:46 +0530

    Second

---
+++ POM/src/main/java/AppPages/HomPage.java
@@ -0,0 +1,13 @@
+package AppPages;
+
+import org.testng.annotations.Test;
+
+public class HomPage extends resusable.BaseClass {
+	
+	@Test
+	public void test() {
+	logger.info("Hi");
+	
+	
+	}
+}
---
+++ POM/src/main/java/resusable/BaseClass.java
@@ -0,0 +1,14 @@
+package resusable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BaseClass {
+	
+		public static Logger logger = LogManager.getLogger(BaseClass.class);
+
+
+
+
+
+}
